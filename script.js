f0ef4a2f418f9bf94a5f8e357ad583d1110140e4
BulkSkurk <adamtimra@hotmail.com>
2025-04-21 23:00:23 +0200

    add sections and images

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ function closeImage() {
   const lightbox = document.getElementById('lightbox');
   lightbox.classList.remove('show');
 }
+
+function openTerms() {
+  document.getElementById('termsModal').style.display = 'block';
+}
+
+function closeTerms() {
+  document.getElementById('termsModal').style.display = 'none';
+}
+
+window.onclick = function (event) {
+  const modal = document.getElementById('termsModal');
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+};
+
+fetch('terms.html')
+  .then((res) => res.text())
+  .then((data) => {
+    document.getElementById('termsModal').innerHTML = data;
+  });
